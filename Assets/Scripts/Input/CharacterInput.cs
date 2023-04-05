56eeb35d3b9d255ad43ef3b1e6d9a557618cae4f
usama-riaz-1010 <usama.riaz.1010@outlook.com>
2023-04-06 01:11:15 +0500

    feature: update new input system functionality to allow decoupling

--- Assets/Scripts/Input/CharacterInput.cs
+++ Assets/Scripts/Input/CharacterInput.cs
@@ -2,16 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.PlayerLoop;
 
 public class CharacterInput : MonoBehaviour
 {
-    [SerializeField] private ThirdPersonControllerActions movementControls;
+    [SerializeField] private ThirdPersonControllerActions thirdPersonControllerActions;
+    [SerializeField] private float speedMultiplier;
     [SerializeField] private Vector2 inputValues;
 
     void OnEnable()
     {
-        movementControls = new ThirdPersonControllerActions();
-        movementControls.Player.Movement.performed += i => inputValues = i.ReadValue<Vector2>();
-        movementControls.Enable();
+        thirdPersonControllerActions = new ThirdPersonControllerActions();
+        thirdPersonControllerActions.Player.Movement.performed += SetInputValues;
+        thirdPersonControllerActions.Player.Movement.Enable();
+    }
+
+    void OnDisable()
+    {
+        thirdPersonControllerActions.Player.Movement.performed -= SetInputValues;
+    }
+
+    void SetInputValues(InputAction.CallbackContext context)
+    {
+        inputValues = context.ReadValue<Vector2>();
+    }
+    void Update()
+    {
+        var pos = transform.position;
+        pos = new Vector3(pos.x + (inputValues.x * speedMultiplier),0,pos.z + (inputValues.y * speedMultiplier));
+        transform.position = pos;
     }
 }
---
+++ Assets/Scripts/Input/ThirdPersonCharacter.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacter : MonoBehaviour
+{
+    [SerializeField] private CharacterInput characterInput;
+}
